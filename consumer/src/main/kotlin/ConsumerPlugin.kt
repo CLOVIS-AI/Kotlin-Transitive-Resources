ddfad0c54a45b6559f83178511aa68daf3a507e9
Frederik Bertling <frederik.bertling@gmail.com>
2024-02-15 16:35:48 +0100

    feat(consumer): Added an extension with a directory parameter to specify the directory to which the resource artifacts are extracted.
    Fixes #6

--- consumer/src/main/kotlin/ConsumerPlugin.kt
+++ consumer/src/main/kotlin/ConsumerPlugin.kt
@@ -2,12 +2,21 @@ package opensavvy.gradle.resources.consumer
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.create
 
 @Suppress("unused")
 class ConsumerPlugin : Plugin<Project> {
 	override fun apply(target: Project) {
+		val extension: ConsumerPluginExtension = target.extensions
+			.create(
+				"kotlinJsResConsumer",
+				ConsumerPluginExtension::class
+			).apply {
+				directory.convention("imported")
+			}
+
 		target.pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
-			target.initializeForKotlin()
+			target.initializeForKotlin(extension)
 		}
 	}
 }
---
+++ examples/kotlin-multiplatform/app-custom-dir/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	kotlin("multiplatform")
+	id("dev.opensavvy.resources.consumer") version "CHANGE-THIS"
+}
+
+repositories {
+	mavenCentral()
+}
+
+kotlin {
+	js(IR) {
+		browser()
+		binaries.executable()
+	}
+
+	sourceSets.jsMain.dependencies {
+		implementation(project(":core"))
+	}
+}
+
+dependencies {
+	transitiveJsResources(project(":core"))
+}
+
+kotlinJsResConsumer {
+	directory = "customDir"
+}
+
