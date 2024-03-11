eddda3537d18ff3e75105d79f68c6f4b050a87a5
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-03-11 21:57:00 +0100

    test(example-kmp): Explain the manual tests in the UI

--- examples/kotlin-multiplatform/app/src/jsMain/kotlin/Main.kt
+++ examples/kotlin-multiplatform/app/src/jsMain/kotlin/Main.kt
@@ -4,8 +4,9 @@ import kotlinx.browser.document
 
 fun main() {
 	document.getElementById("root")!!.innerHTML = """
-		<div class="blue">Loaded Blue</div>
-		<div class="yellow">Loaded Yellow</div>
-		<div class="red">Loaded Red</div>
+		<div class="blue">This should be blue (from core/commonMain)</div>
+		<div class="yellow">This should be yellow (from core/webMain)</div>
+		<div class="red">This should be red (from core/jsMain)</div>
+		<div class="green">This should NOT be green (from core/wasmJsMain)</div>
 	""".trimIndent()
 }
