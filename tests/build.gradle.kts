eaf9db5a4093dbde7d12e4d5a48bf28fdead1f2d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-07-13 17:06:45 +0200

    test: Migrate to TestBalloon

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	alias(opensavvyConventions.plugins.base)
 	alias(opensavvyConventions.plugins.plugin)
+	alias(libsCommon.plugins.testBalloon)
 }
 
 dependencies {
@@ -11,7 +12,7 @@ dependencies {
 		because("Without it, any usage of any symbol from the Kotlin plugin results in test projects not being configured at all.")
 	}
 
-	testImplementation(libsCommon.opensavvy.prepared.kotlinTest)
+	testImplementation(libsCommon.opensavvy.prepared.testBalloon)
 	testImplementation(libsCommon.opensavvy.prepared.gradle)
 }
 
